f14605d1b92f634e979da4d488fc6daaaed99ba8
Philip <Philipkd1112@gmail.com>
2020-08-05 13:06:26 +0800

    final

---
+++ app/src/main/java/com/philip/whyhomework/MainActivity.java
@@ -0,0 +1,104 @@
+package com.philip.whyhomework;
+
+import android.graphics.Bitmap;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import org.opencv.android.InstallCallbackInterface;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.aruco.Aruco;
+import org.opencv.aruco.Dictionary;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.QRCodeDetector;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgView;
+    TextView txtView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("Philip", "dead");
+        } else {
+            Log.d("Philip", " alive");
+        }
+
+        imgView = findViewById(R.id.imageView);
+        txtView = findViewById(R.id.textView);
+
+        Log.d("Philip", " save");
+        Button butt = findViewById(R.id.button);
+
+
+        butt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                final Bitmap bmm = ((BitmapDrawable) imgView.getDrawable()).getBitmap();
+
+                Mat mat = new Mat(bmm.getWidth(), bmm.getHeight(), CvType.CV_8UC4);
+                Utils.bitmapToMat(bmm, mat);
+                String ans = decodeQRcode(bmm);
+                String[] output = ans.split(";");
+                Scalar lineColor = new Scalar(255,0,0,255);
+                int lineWidth = 3;
+
+                for(int i = 0 ; i<output.length ; i++){
+                    Point[]points={new Point(), new Point()};
+                    String [] Space = output[i].split(" ");
+                    for(int j = 0 ; j < Space.length ; j++){
+                        String [] abc = Space[j].split(",");
+                        points[j] = new Point(Integer.parseInt(abc[0]),Integer.parseInt(abc[1]) );
+
+                    }
+                    Imgproc.line(mat, points[0], points[1], lineColor, lineWidth);
+                    //Imgproc.line(mat, points[1], points[2], lineColor, lineWidth);
+                    //Imgproc.line(mat, points[2], points[3], lineColor, lineWidth);
+                    //Imgproc.line(mat, points[3], points[4], lineColor, lineWidth);
+                    //Imgproc.line(mat, points[4], points[0], lineColor, lineWidth);
+                }
+
+                    Bitmap bitmap = Bitmap.createBitmap(mat.width(), mat.height(), Bitmap.Config.ARGB_8888);
+                    Utils.matToBitmap(mat, bitmap);
+                    ImageView imgView = findViewById(R.id.imageView);
+                    imgView.setImageBitmap(bitmap);
+
+
+
+            }
+        });
+
+    }
+    String decodeQRcode (Bitmap bitmap){
+        Mat mat = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC4);
+        Utils.bitmapToMat(bitmap, mat);
+        QRCodeDetector qrCodeDetector = new QRCodeDetector();
+        String result = qrCodeDetector.detectAndDecode(mat);
+        Log.d("Philip", "result" + result);
+        txtView.setText(result);
+        return result;
+    }
+}
